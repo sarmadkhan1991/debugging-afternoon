513ece6da09382902d5eee466851637a7895dcaf
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2019-11-26 16:12:39 -0700

    black-diamond done!

--- black-diamond/src/App.js
+++ black-diamond/src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
       cart: [],
       showCart: false
     };
+    this.addToCart = this.addToCart.bind(this);
+    this.removeFromCart= this.removeFromCart.bind(this);
+    this.navigate = this.navigate.bind(this);
   }
   componentDidMount() {
     axios
@@ -36,13 +39,14 @@ class App extends Component {
   }
   navigate(location) {
     if (location === "cart") {
-      this.state.showCart = true;
+      this.setState({showCart: true});
     } else {
-      this.state.showCart = false;
+      this.setState({showCart: false});
     }
   }
   render() {
     const { products, cart, showCart } = this.state;
+    console.log(products);
     return (
       <div className="App">
         <NavBar navigate={this.navigate} />
